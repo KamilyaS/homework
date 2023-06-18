b295876ea6aaa452aa01a294c4d541c254d78347
Kamilya <you@example.com>
2023-06-18 08:12:22 +0300

    Решение задачи 3

---
+++ task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход число и выдает, является ли число четным (делится ли оно на 2 без остатка)
+4 -> да
+-3 -> нет
+7 -> нет */
+
+Console.WriteLine("Add numberA ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0) 
+    Console.Write("Yes");
+else
+{
+    Console.Write("No");
+}
